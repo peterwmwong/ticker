7eafbb5c62eb3902e9a11e2fe09ec97ef200b300
peterwmwong <peter.wm.wong@gmail.com>
2014-07-30 18:05:06 -0500

    Use core-scroll-header-panel

--- src/elements/ticker-app.js
+++ src/elements/ticker-app.js
@@ -1,2 +1,17 @@
 Polymer('ticker-app',{
+  ready(){
+    window.addEventListener('core-header-transform',this.onCoreHeaderTransform.bind(this));
+  },
+
+  onCoreHeaderTransform(e){
+    // custom transformation: scale header's title
+    var titleStyle = this.$.titleText.style;
+    var d = e.detail;
+    var m = d.height - d.condensedHeight;
+    var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
+    titleStyle.webkitTransform = titleStyle.transform =
+        'scale(' + scale + ') translateZ(0)';
+    console.log('scaling');
+  }
+
 });
