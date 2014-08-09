840b3852c372cf11eaf860fb96b91c8098671f61
peterwmwong <peter.wm.wong@gmail.com>
2014-08-08 19:54:29 -0500

    Remove accidentally added files

--- spec/elements/ElementInterface.js
+++
@@ -1,6 +0,0 @@
-export default class ElementInterface {
-  constructor(el){ this.el = el; }
-
-  qs(selector) { return this.el.shadowRoot.querySelector(selector); }
-  qsa(selector){ return this.el.shadowRoot.querySelectorAll(selector); }
-}
