817d3cad1f3483cffe27308684146cde9f8134dd
peterwmwong <peter.wm.wong@gmail.com>
2014-08-28 18:14:27 -0500

    New refresh button and fixed height header bar

--- src/elements/ticker-app.js
+++ src/elements/ticker-app.js
@@ -1,19 +1,15 @@
 import Event from '../models/github/Event';
 
 Polymer('ticker-app',{
+  query: {type:'users',users:'polymer'},
+
   ready(){
     // this.githubEvents = Event.query({type:'repos', repos:'centro/centro-media-manager'});
-    this.githubEvents = Event.query({type:'users', users:'polymer'});
+    this.githubEvents = Event.query(this.query);
   },
 
-  onCoreHeaderTransform(e){
-    // custom transformation: scale header's title
-    var titleStyle = this.$.titleText.style;
-    var d = e.detail;
-    var m = d.height - d.condensedHeight;
-    var scale = Math.max(0.75, (m - d.y) / (m / 0.25)  + 0.75);
-    titleStyle.webkitTransform = titleStyle.transform =
-        'scale(' + scale + ') translateZ(0)';
+  // Event Handlers
+  onRefresh(){
+    this.githubEvents.$query(this.query);
   }
-
 });
