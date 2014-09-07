51515789dcbb82baa69769772f0e901ce45ac507
peterwmwong <peter.wm.wong@gmail.com>
2014-09-07 18:21:58 -0500

    Add EventStream (should have been in previous commit)

---
+++ src/models/EventStream.js
@@ -0,0 +1,39 @@
+import AttrMunger  from 'helpers/AttrMunger';
+import Model       from '../helpers/model/Model';
+import GithubEvent from './github/GithubEvent';
+import GithubUser from './github/GithubUser';
+
+class EventStream extends Model {
+  name(){ throw 'Implement me'; }
+  events(){ throw 'Implement me'; }
+}
+
+EventStream.create($=>{
+  $.attr('type', 'string');
+  $.attr('config', 'identity');
+  $.mapper = {
+    query: (array, {term})=>
+      GithubUser.query({q:term}).$promise
+        .then(users=>
+          array.$replace(
+            users.map(user=>
+              GithubEventStream.load({
+                id: user.id,
+                config: {
+                  type: 'users',
+                  users: user.login
+                }
+              }))))
+  };
+});
+
+export class GithubEventStream extends EventStream {
+  get name(){
+    return this.config[this.config.type];
+  }
+  events(){
+    return GithubEvent.query(this.config);
+  }
+}
+
+export default EventStream;
