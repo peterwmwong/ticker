bcbac2a734a45395152265ce0bfe3ce67d68053e
peterwmwong <peter.wm.wong@gmail.com>
2014-11-03 08:54:20 -0600

    Leverage async functions

--- src/models/github/GithubEventMapper.js
+++ src/models/github/GithubEventMapper.js
@@ -1,7 +1,6 @@
 import AttrMunger from 'helpers/AttrMunger';
 import loadJSON from 'helpers/load';
 
-
 // !!! <MOCKDATA> !!!
   // import MOCKDATA from './GithubEventMapperMOCKDATA';
   // import MOCKDATA2 from './GithubEventMapperMOCKDATA2';
@@ -9,14 +8,14 @@ import loadJSON from 'helpers/load';
 // !!! </MOCKDATA> !!!
 
 export default {
-  query:(array,{type, [type]:typeRef})=>
-    (
-      loadJSON(`https://api.github.com/${type}/${typeRef}/events`)
-      // Promise.resolve(MOCKDATA)
-      // Promise.resolve([])
-      // Promise.resolve([MOCKDATA, MOCKDATA2][(window.mocki = ((window.mocki || 0)+1)%2)])
-    ).then(data=>
-        array.$replace(
-          array.$class.loadAll(
-            AttrMunger.camelize(data))))
+  query: async (array,{type, [type]:typeRef})=>
+    array.$replace(
+      array.$class.loadAll(
+        AttrMunger.camelize(
+          await
+            loadJSON(`https://api.github.com/${type}/${typeRef}/events`)
+            // Promise.resolve(MOCKDATA)
+            // Promise.resolve([])
+            // Promise.resolve([MOCKDATA, MOCKDATA2][(window.mocki = ((window.mocki || 0)+1)%2)])
+        )))
 };
