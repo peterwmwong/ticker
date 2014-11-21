7f1f0b44ff5ce17350c45d165f23f7b58675d426
peterwmwong <peter.wm.wong@gmail.com>
2014-11-20 21:05:59 -0600

    Clean up search

--- src/elements/ticker-search.js
+++ src/elements/ticker-search.js
@@ -2,19 +2,5 @@ import StatefulPolymer from '../helpers/StatefulPolymer';
 import appState from '../states/appState';
 
 StatefulPolymer('ticker-search', {
-  state:appState,
-
-  // Filters
-  // =======
-
-  onSearchTextChanged:{
-    toDOM:searchText=>searchText,
-    toModel(searchText){
-      this.job('ticker-search',()=>{
-        this.stateEvent('searchTextChanged', searchText);
-      }, 500);
-      return this.state.searchText;
-    }
-  }
-
+  state:appState
 });
