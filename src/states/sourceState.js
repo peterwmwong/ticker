6e505cfa5307a9494d577138a6c95e6c1b3fb6b7
peterwmwong <peter.wm.wong@gmail.com>
2014-11-22 21:16:57 -0600

    Resurrect/Fix adding a Github User as a favorite

--- src/states/sourceState.js
+++ src/states/sourceState.js
@@ -12,11 +12,11 @@ export default {
     'selectSource':source=>reenter({source}),
     'toggleFavoriteSource'(){
       var {user,source} = this.attrs;
-      if(this.attrs.isStreamFavorited){
-        if(user.sources.indexOf(source) !== -1) user.sources.push(source);
+      if(!this.attrs.isSourceFavorited){
+        if(user.sources.indexOf(source) === -1) user.sources.push(source);
       }else{
         var index = user.sources.indexOf(source);
-        if(index === -1) user.sources.splice(index, 1);
+        if(index !== -1) user.sources.splice(index, 1);
       }
       user.$save();
       return reenter({source});
