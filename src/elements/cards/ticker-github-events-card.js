8c9cd1ce820a8e31fa11ef943d945ec00189970c
peterwmwong <peter.wm.wong@gmail.com>
2015-01-04 16:36:32 -0600

    Show <ticker-github-commit> Overlay View when the user selects a commit from the <ticker-github-events-card>

---
+++ src/elements/cards/ticker-github-events-card.js
@@ -0,0 +1,6 @@
+import StatefulPolymer from '../../helpers/StatefulPolymer.js';
+import appState from '../../states/appState.js';
+
+StatefulPolymer('ticker-github-events-card', {
+  state:appState
+});
