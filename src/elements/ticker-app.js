b11da0e36b72f1b484b3c9a6e3b057a4d8c4e6ca
peterwmwong <peter.wm.wong@gmail.com>
2015-05-04 13:40:10 -0500

    Add todo to add back authWithGithub

--- src/elements/ticker-app.js
+++ src/elements/ticker-app.js
@@ -5,6 +5,10 @@ Polymer({
     syncState(['appView', 'isLoggedIn'])
   ],
 
-  _onToggleDrawer(e){ this.$.drawer.togglePanel(); },
-  _onAuthWithGithub:()=>appState.fire('authWithGithub')
+  _onToggleDrawer(e){ this.$.drawer.togglePanel(); }
+
+  // TODO(pwong): Currently unused but will need to sprinkle this all over the
+  //              place.  Wherever the user needs to do a user specfic operation
+  //              (ex. Favorite a Github User/Repo)
+  // _onAuthWithGithub:()=>appState.fire('authWithGithub')
 });
