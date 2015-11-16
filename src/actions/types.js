dea1f3c21644b4f8dde5e7621a2f4e5cc24405fd
peterwmwong <peter.wm.wong@gmail.com>
2015-11-16 07:30:37 -0600

    Remove old redux related code

--- src/actions/types.js
+++
@@ -1,5 +0,0 @@
-export const ADD_EVENT   = 'ADD_EVENT';
-
-export const APP_SETVIEW = 'APP_SETVIEW';
-
-export const USER_LOADED = 'USER_LOADED';
--- src/store.js
+++
@@ -1,6 +0,0 @@
-import thunk from 'redux-thunk';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import * as reducers from './reducers';
-
-const store = window.store = applyMiddleware(thunk)(createStore)(combineReducers(reducers));
-export default store;
