27df6fefec6e685650d6d6f0f610602bb05fb7f0
peterwmwong <peter.wm.wong@gmail.com>
2016-01-27 08:10:52 -0600

    Simplify RepoView state
    Instead of allocating an object just to hold single repo object.

--- src/components/RepoView.jsx
+++ src/components/RepoView.jsx
@@ -2,12 +2,12 @@ import './RepoView.css';
 import GithubRepo from '../models/github/GithubRepo';
 import EventsView from './EventsView.jsx';
 
-const RepoView = ({id, type}, {repo})=>
+const RepoView = ({id, type}, repo)=>
   <div className="RepoView">
     <div className="App__content Card Card--fullBleed bg-purple">
       <div className="Card-title">
         <h1 className="c-white t-word-wrap-break-word" textContent={id} />
-        <span className="c-gray-lighter t-light" textContent={repo && repo.description} />
+        <span className="c-white t-light" textContent={repo && repo.description} />
       </div>
     </div>
     <EventsView id={id} type={type} />
@@ -21,7 +21,7 @@ const init = ({id}, state, {loadRepo})=>(
 RepoView.state = {
   onInit: init,
   onProps: init,
-  loadRepo: (props, state, actions, repo)=>({repo})
+  loadRepo: (props, state, actions, repo)=>repo
 }
 
 export default RepoView;
