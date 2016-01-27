6d58ec08cf0ca5d44f33ffa7d157083b47330a1a
peterwmwong <peter.wm.wong@gmail.com>
2016-01-27 08:12:09 -0600

    Simplify EventsView state
    Don’t allocate an object just to hold an array of events.

--- src/components/EventsView.jsx
+++ src/components/EventsView.jsx
@@ -6,24 +6,23 @@ const EVENT_TYPES_TO_HIDE = {
   'GollumEvent': true
 };
 
-const EventsView = (props, state)=>
+const EventsView = (props, events)=>
   <div className="l-margin-t2">
-    {state.events.map(event=>
+    {events.map(event=>
       <EventCard key={event.id} event={event} recycle />
     )}
   </div>;
 
 const onInit = (props, state, {loadEvents})=>(
   GithubEvent.query(props).then(loadEvents),
-  {events: loadEvents(GithubEvent.localQuery(props)).events}
+  loadEvents(GithubEvent.localQuery(props))
 );
 
 EventsView.state = {
   onInit: onInit,
   onProps: onInit,
-  loadEvents: (props, state, actions, events)=>({
-    events: events.filter(e=>!EVENT_TYPES_TO_HIDE[e.type])
-  })
+  loadEvents: (props, state, actions, events)=>
+    events.filter(e=>!EVENT_TYPES_TO_HIDE[e.type])
 };
 
 export default EventsView;
