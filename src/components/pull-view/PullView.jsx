57815e004fc7b8d8f4af22f401d512f89b43d7a1
peterwmwong <peter.wm.wong@gmail.com>
2016-03-31 08:07:13 -0500

    PullView displays PR title in toolbar

--- src/components/pull-view/PullView.jsx
+++ src/components/pull-view/PullView.jsx
@@ -1,9 +1,10 @@
 import xvdom           from 'xvdom';
-import AppToolbar      from '../AppToolbar.jsx';
-import Tabs            from '../common/Tabs.jsx';
-import IssuePullInfo   from '../IssuePullInfo.jsx';
 import PullCommitsView from './PullCommitsView.jsx';
 import PullDiffView    from './PullDiffView.jsx';
+import AppToolbar      from '../AppToolbar.jsx';
+import IssuePullInfo   from '../IssuePullInfo.jsx';
+import Tabs            from '../common/Tabs.jsx';
+import GithubIssue     from '../../models/github/GithubIssue';
 
 const TABS = {
   info:{
@@ -20,15 +21,29 @@ const TABS = {
   }
 };
 
-export default ({pullId, repo, tab='info'})=>
+const PullView = ({pullId, repo, tab='info'}, pull)=>
   <div>
     <AppToolbar
       secondary={
         <Tabs hrefPrefix={`#github/${repo}?pulls/${pullId}/`} selected={tab} tabs={TABS} />
       }
-      title={pullId}
+      title={`PR #${pullId}: ${pull.title}`}
     />
     <div className='l-padding-t24 l-padding-b2'>
       {TABS[tab].view(repo, pullId)}
     </div>
-  </div>
+  </div>;
+
+const onInit = ({repo, pullId}, state, {loadIssue})=> {
+  const id = `${repo}/${pullId}`;
+  GithubIssue.get(id).then(loadIssue);
+  return GithubIssue.localGet(id)
+};
+
+PullView.state = {
+  onInit,
+  onProps: onInit,
+  loadIssue: (props, state, actions, pull)=> pull
+};
+
+export default PullView;
