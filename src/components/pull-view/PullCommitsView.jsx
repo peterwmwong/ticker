ae7a6a0cd910f6e3869fa15d3fbc59468a9b4450
peterwmwong <peter.wm.wong@gmail.com>
2016-03-31 07:54:10 -0500

    Handle non-Github user commits
    Also, reduce arrow function allocation

--- src/components/pull-view/PullCommitsView.jsx
+++ src/components/pull-view/PullCommitsView.jsx
@@ -1,19 +1,25 @@
 import xvdom            from 'xvdom';
 import GithubIcon       from '../common/GithubIcon.jsx';
+import Avatar           from '../common/Avatar.jsx';
 import GithubPullCommit from '../../models/github/GithubPullCommit.js';
 import compare          from '../../helpers/compare';
 import timeAgo          from '../../helpers/timeAgo';
 
+const sortCreatedAt = (a, b)=> compare(b.created_at, a.created_at);
+
 const PullCommitsView = ({id, repo}, commits)=>
   <div className='l-margin-t2 Card' hidden={!commits.length} >
-    {commits.map(({sha, author, commit:{committer, message}})=>
+    {commits.map(({sha, author, commit:{author:{name:authorName}, committer, message}})=>
       <div className='List-item layout horizontal center' key={sha}>
-        <GithubIcon className='l-margin-r3' name='git-commit' />
-        <a className='t-normal' href={`#github/${repo}?commits/${sha}`}>
+        {author
+          ? <Avatar avatarUrl={author.avatar_url} />
+          : <GithubIcon name='person' />
+        }
+        <a className='l-margin-l3 t-normal' href={`#github/${repo}?commits/${sha}`}>
           {message}
           <div
             className='t-light t-font-size-14 c-gray-dark'
-            textContent={`committed ${timeAgo(Date.parse(committer.date))} ago by ${author.login}`}
+            textContent={`committed ${timeAgo(Date.parse(committer.date))} ago by ${author ? author.login : authorName}`}
           />
         </a>
       </div>
@@ -25,8 +31,7 @@ PullCommitsView.state = {
     GithubPullCommit.query(props).then(loadCommits),
     []
   ),
-  loadCommits: (props, state, actions, commits)=>
-    commits.sort((a, b)=> compare(b.created_at, a.created_at))
+  loadCommits: (props, state, actions, commits)=> commits.sort(sortCreatedAt)
 }
 
 export default PullCommitsView;
