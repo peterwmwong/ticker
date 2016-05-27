4be505a036150b69bc8c3e628e898d9437dc54e6
peterwmwong <peter.wm.wong@gmail.com>
2016-05-27 17:22:30 -0500

    [sw] tweak service worker cache app path

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 const CACHE_VERSION = 1;
 const CACHE_NAME = `static-v${CACHE_VERSION}`;
-const INDEX_PATH_NAME = '/dist/';
+const INDEX_PATH_NAME = '/ticker/dist/';
 const CACHE = caches.open(CACHE_NAME);
 
 self.addEventListener('install', (event)=> {
