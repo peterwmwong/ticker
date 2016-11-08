c3492ca3bea132f33b5c39bb39f8dc493468e892
peterwmwong <peter.wm.wong@gmail.com>
2016-11-08 00:00:31 -0600

    Fix Service Worker not caching App.min.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,11 +1,14 @@
-const CACHE_VERSION = 17;
+const CACHE_VERSION = 18;
 const CACHE_NAME = `static-v${CACHE_VERSION}`;
-const INDEX_PATH_NAME = '/ticker/dist/';
+const CACHED_PATH_NAMES = [
+  '/ticker/dist/',
+  '/ticker/dist/App.min.js'
+]
 const CACHE = caches.open(CACHE_NAME);
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    CACHE.then(cache => cache.addAll([INDEX_PATH_NAME]))
+    CACHE.then(cache => cache.addAll(CACHED_PATH_NAMES))
   )
 });
 
@@ -23,7 +26,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   const pathname = new URL(event.request.url).pathname;
-  if(pathname === INDEX_PATH_NAME){
+  if(CACHED_PATH_NAMES.indexOf(pathname) !== -1){
     event.respondWith(
       CACHE.then(cache => cache.match(pathname))
     );
